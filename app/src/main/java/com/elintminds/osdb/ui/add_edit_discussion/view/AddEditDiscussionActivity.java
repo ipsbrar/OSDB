11575c65a2ccd877e92bc0b1a90a62302d2b8936
Joga Singh <mac@MACs-MacBook-Pro.local>
2019-03-07 10:20:39 +0530

    add discussion keyboard hide implemented

--- app/src/main/java/com/elintminds/osdb/ui/add_edit_discussion/view/AddEditDiscussionActivity.java
+++ app/src/main/java/com/elintminds/osdb/ui/add_edit_discussion/view/AddEditDiscussionActivity.java
@@ -1,21 +1,26 @@
 package com.elintminds.osdb.ui.add_edit_discussion.view;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.elintminds.osdb.R;
 import com.elintminds.osdb.ui.base.view.BaseActivity;
 import com.elintminds.osdb.ui.login.view.LoginActivity;
 import com.elintminds.osdb.ui.register.view.RegisterActivity;
+import com.elintminds.osdb.utils.CustomEditText;
 
 public class AddEditDiscussionActivity extends BaseActivity implements View.OnClickListener {
 
     private TextView addDiscussionBtn;
     private ImageView backImg;
-
+private CustomEditText customEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -29,6 +34,19 @@ public class AddEditDiscussionActivity extends BaseActivity implements View.OnCl
     {
         addDiscussionBtn = findViewById(R.id.add_btn);
         backImg = findViewById(R.id.backImg);
+        customEditText = findViewById(R.id.txt_add_comment);
+
+        customEditText.setKeyImeChangeListener(new CustomEditText.KeyImeChange() {
+            @Override
+            public void onKeyIme(int keyCode, KeyEvent event) {
+                if (KeyEvent.KEYCODE_BACK == event.getKeyCode())
+                {
+                    // do something
+                    Log.e("AddDiscussion_activity","KeyEvent");
+                    finish();
+                }
+            }
+        });
 
         addDiscussionBtn.setOnClickListener(this);
         backImg.setOnClickListener(this);
@@ -53,7 +71,19 @@ public class AddEditDiscussionActivity extends BaseActivity implements View.OnCl
 
     @Override
     public void onBackPressed() {
+        Log.e("AddDiscussion_activity","onBackPressed");
         super.onBackPressed();
         finish();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Checks whether a hardware keyboard is available
+        if (newConfig.hardKeyboardHidden == Configuration.HARDKEYBOARDHIDDEN_NO) {
+            Toast.makeText(this, "keyboard visible", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.hardKeyboardHidden == Configuration.HARDKEYBOARDHIDDEN_YES) {
+            Toast.makeText(this, "keyboard hidden", Toast.LENGTH_SHORT).show();
+        }
     }
 }
